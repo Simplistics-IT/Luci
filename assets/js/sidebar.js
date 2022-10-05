8c16bf459f2a4c11ee08b03ec76325e8d0657da5
Simplistics-IT <113269089+Simplistics-IT@users.noreply.github.com>
2022-10-05 11:00:35 -0500

    Modificación de los módulos
    Se modificaron todos los módulos para que pudieran ser solo archivos HTML y se les incluyeran el sidebar, navbar y footer utilizando vanilla js mediante un script

---
+++ assets/js/sidebar.js
@@ -0,0 +1,11 @@
+// const container_sidebar = document.getElementById('layoutSidenav_nav');
+
+// if (container_sidebar) {
+// 	const links = container_sidebar.querySelectorAll('#sidenavAccordion > div.sb-sidenav-menu > div.nav > a.nav-link');
+
+// 	for (let link of links) {
+// 		if (link.href.trim() === window.location.href) {
+// 			link.className = 'nav-link active';
+// 		}
+// 	}
+// }
