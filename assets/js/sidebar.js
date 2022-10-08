fb6f78f96ec7c33260a9817d59fd8a1733e0a58a
Simplistics-IT <113269089+Simplistics-IT@users.noreply.github.com>
2022-10-08 13:54:39 -0500

    Add files via upload
    Se reestructuró el header, footer y sidebar para tener un diseño más agradable y con un script que oculte y muestr el sidebar al pulsar un botón

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,11 +1,36 @@
-// const container_sidebar = document.getElementById('layoutSidenav_nav');
+window.addEventListener('DOMContentLoaded', event => {
 
-// if (container_sidebar) {
-// 	const links = container_sidebar.querySelectorAll('#sidenavAccordion > div.sb-sidenav-menu > div.nav > a.nav-link');
+    const showSidebar = (btnToggleId, hideText, asideWidth, paddingChange, smallLogo) => {
+        const btnToggle = document.getElementById(btnToggleId),
+            hideElem = document.getElementsByClassName(hideText),
+            elemAsideWidth = document.querySelector(asideWidth),
+            elemPaddingChange = document.getElementsByClassName(paddingChange),
+            smallLogoDisplay = document.getElementById(smallLogo);
+            btnToggle.addEventListener('click', () => {
+                for (let i = 0; i < hideElem.length; i++) {
+                    hideElem[i].classList.toggle('hide-elem');
+                }
+                elemAsideWidth.classList.toggle('aside-change');
+                for (let i = 0; i < elemPaddingChange.length; i++) {
+                    elemPaddingChange[i].classList.toggle('padding-change');
+                }
+                smallLogoDisplay.classList.toggle('small-logo-hiden');
+            });
+    }
+    
+    showSidebar('btn-sidebar-toggle', 'hide-text', '.aside' ,'padding-base', 'sm-logo');
 
-// 	for (let link of links) {
-// 		if (link.href.trim() === window.location.href) {
-// 			link.className = 'nav-link active';
-// 		}
-// 	}
-// }
+
+	/*===== LINK ACTIVE =====
+	const linkColor = document.querySelectorAll('.nav_link')
+
+	function colorLink() {
+		if (linkColor) {
+			linkColor.forEach(l => l.classList.remove('active'))
+			this.classList.add('active')
+		}
+	}
+	linkColor.forEach(l => l.addEventListener('click', colorLink))*/
+
+	// Your code to run since DOM is loaded and ready
+});
