c82a52f04f1f3d8730609614b2cad6743086bb28
CylanTomacio <elvisdanielbriceno@gmail.com>
2022-10-11 08:27:29 -0500

    Se creo el login y se modifico el botón del sidebar para que lo oculte y lo aparezca. Además se cambió el lugar en donde se ve al usuario conectado

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,36 +1,21 @@
-window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener('DOMContentLoaded', event => { //Ejecutamos cuando todo se haya cargado
 
-    const showSidebar = (btnToggleId, hideText, asideWidth, paddingChange, smallLogo) => {
-        const btnToggle = document.getElementById(btnToggleId),
-            hideElem = document.getElementsByClassName(hideText),
-            elemAsideWidth = document.querySelector(asideWidth),
-            elemPaddingChange = document.getElementsByClassName(paddingChange),
-            smallLogoDisplay = document.getElementById(smallLogo);
+    const showSidebar = (btnToggleId, btnIconId, asideWidth, paddingChange) => { //Obtenemos los valores de los elementos que se modificaran
+        const btnToggle = document.getElementById(btnToggleId),//Botón del menú
+            btnIcon = document.getElementById(btnIconId),//Ícono del botón de menú
+            elemAsideWidth = document.querySelector(asideWidth),//Elemento aside
+            elemPaddingChange = document.getElementsByClassName(paddingChange);//Elementos a los que se les cambiará el padding
             btnToggle.addEventListener('click', () => {
-                for (let i = 0; i < hideElem.length; i++) {
-                    hideElem[i].classList.toggle('hide-elem');
-                }
+                //Cambiamos el ícono del menú cuando está abierto
+                btnIcon.classList.toggle('fa-xmark');
+                //Abrimos el sidebar cuando se pulsa el botón de menú
                 elemAsideWidth.classList.toggle('aside-change');
+                //Cambiamos el padding del navbar, main y footer para adaptarse cuando el menú está abierto
                 for (let i = 0; i < elemPaddingChange.length; i++) {
                     elemPaddingChange[i].classList.toggle('padding-change');
                 }
-                smallLogoDisplay.classList.toggle('small-logo-hiden');
             });
     }
     
-    showSidebar('btn-sidebar-toggle', 'hide-text', '.aside' ,'padding-base', 'sm-logo');
-
-
-	/*===== LINK ACTIVE =====
-	const linkColor = document.querySelectorAll('.nav_link')
-
-	function colorLink() {
-		if (linkColor) {
-			linkColor.forEach(l => l.classList.remove('active'))
-			this.classList.add('active')
-		}
-	}
-	linkColor.forEach(l => l.addEventListener('click', colorLink))*/
-
-	// Your code to run since DOM is loaded and ready
+    showSidebar('btn-sidebar-toggle', 'btn-sidebar-icon' , '.aside' ,'padding-base');// Llamamos a la función que muestra el sidebar con los valores de id o clases como parametros
 });
