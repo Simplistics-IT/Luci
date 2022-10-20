c7f570936d77674bb9fb68f7ad4994a95750ebcc
CylanTomacio <elvisdanielbriceno@gmail.com>
2022-10-20 13:25:17 -0500

    Se agregaron varias funciones para obtener la info del formulario y convertirlas en JSON

--- assets/js/ingreso-manual.js
+++ assets/js/ingreso-manual.js
@@ -18,7 +18,10 @@ const inputPrecio = document.getElementById('input-precio');
 const btnEliminarProducto = document.querySelector('.btn-eliminar-producto');
 const btnAgregarProducto = document.getElementById('btn-agregar-producto');
 let portafolioList = [];
-let dataToJson = {};
+let dataToJson = {
+    Quantity: 0,
+    Total: 0
+};
 
 /* Variables para enviar la info */
 const btnSendManualOrder = document.getElementById('btn-agregar-pedido');
@@ -27,35 +30,48 @@ const inputDireccion = document.querySelectorAll('.direccion');
 const inputProductos = document.getElementsByClassName('producto');
 
 function sendManualOrder() {
-    let direccion = [];
-    let productosToJson = [];
-    let obj = {};
+    let direccion = [];//Array con los datos de la dirección para ser concatenada
+    let productosToJson = [];//Array de objetos donde se almacenan los productos
+    let objProducto = {};//Con este objeto almaceno cada producto recibido del formulario y luego lo guardo en el arreglo productosToJson
+
     inputDireccion.forEach( elem => {
         if (elem.value != '')
             direccion.push( elem.value );
         else
             console.warn('No dejes campos vacíos');
-    });
+    });//Para almacenar los datos de la direccion
+
     inputsIngresoManual.forEach( input => {
         if (input.value != '' && input.name != '')
             dataToJson[input.name] = input.value;
         else
             console.warn('No dejes campos vacíos');
-    });
+    });//Para recibir los datos de los campos del formulario
 
     for (const producto of inputProductos) {
         if (producto.name === 'UnitPrice' && producto.value != undefined) {
-            obj[producto.name] = producto.value;
-            productosToJson.push(obj);
-            obj = {};
+            objProducto[producto.name] = producto.value;
+            productosToJson.push(objProducto);
+            objProducto = {};
         } else if (producto.value != undefined) {
-            obj[producto.name] = producto.value;
+            objProducto[producto.name] = producto.value;
         }
-    }
-    dataToJson['Address'] = direccion.join(' ');
-    dataToJson['Products'] = productosToJson;
+    }//Para almacenar los productos como objetos dentro del array principal: dataToJson
 
-    console.log(dataToJson);
+    productosToJson.forEach( producto => {
+        producto.Total = String( Number(producto.Quantity) * Number(producto.UnitPrice) );
+    });//Calculamos el total de cada producto
+
+    dataToJson['Address'] = direccion.join(' ');//Concatenamos la direccion y la guardamos
+    dataToJson['Products'] = productosToJson;//Guardamos los productos en el array principal
+
+
+    dataToJson.Products.map( producto => {
+        dataToJson.Quantity += Number(producto.Quantity);
+        dataToJson.Total += Number(producto.Total);
+    });
+
+    console.log(JSON.stringify(dataToJson));
 }
 
 function agregarProducto() {
@@ -111,7 +127,7 @@ function llenarPortafolio(data) {
 function buscarProducto() {
     portafolioList.forEach( producto => {
         let productoOpcion = document.createElement('option');
-        productoOpcion.value = `${producto.id} - ${producto.item}`;
+        productoOpcion.value = `${producto.id} | ${producto.item}`;
         portafolioOpciones.appendChild(productoOpcion);
     });
 }
